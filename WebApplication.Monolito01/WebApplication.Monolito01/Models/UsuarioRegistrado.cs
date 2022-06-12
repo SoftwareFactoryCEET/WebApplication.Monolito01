8c79bfcf5bb93a24df9f8449760c22d9c59646fa
Fábrica de Software CEET <vcortes@sena.edu.co>
2022-06-12 12:59:45 -0500

    Add class 'UsuarioRegistrado' : IdentityUser

---
+++ WebApplication.Monolito01/WebApplication.Monolito01/Models/UsuarioRegistrado.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication.Monolito01.Models
+{
+    [Table("Usuario")]
+    public class UsuarioRegistrado : IdentityUser
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Pais { get; set; }
+        public string CodigoPais { get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+        public bool Estado { get; set; } = true;
+        public string? URL { get; set; }
+    }
+}
