c1b28321ec2b66862be75a47da57d69787bf3582
Fábrica de Software CEET <vcortes@sena.edu.co>
2022-06-11 23:59:33 -0500

    Add Secrests. Add Services Send Grid and Twilio.

---
+++ WebApplication.Monolito01/WebApplication.Monolito01/Services/SmsOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Monolito01.Services
+{
+    public class SmsOptions
+    {
+        public string SMSAccountIdentification { get; set; }
+        public string SMSAccountPassword { get; set; }
+        public string SMSAccountFrom { get; set; }
+    }
+}
