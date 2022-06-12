83d1a099eca87a991f1ad91d5164e68ee31e9600
Fábrica de Software CEET <vcortes@sena.edu.co>
2022-06-12 17:48:30 -0500

    Custom Register. Add Class Model UsuarioRegistrado.  Add class ViewModel RegisterViewModel. Add Controller Cuenta. Add Actions GET POST and Others... Add Migrations. Add ViewRegister Custom.  Add JQuery UI. Custom Calendar _Layout, add Img, Custom CSS Site.

---
+++ WebApplication.Monolito01/WebApplication.Monolito01/Controllers/CuentasController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Monolito01.Models;
+using WebApplication.Monolito01.Models.ViewModels;
+
+namespace WebApplication.Monolito01.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;        // Manejador de Usuarios
+        private readonly IEmailSender _emailSender;                     // Interfaz para manejo de Email
+        private readonly SignInManager<IdentityUser> _signInManager;    // Manejador de autenticación
+
+
+        // Crear un constructor
+        public CuentasController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registro(string? returnurl = null)
+        {
+            //Pendiente implementar código para crear roles.
+
+            ViewData["ReturnUrl"] = returnurl;
+            var registroViewModel = new RegisterViewModel();
+            return View(registroViewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registro(RegisterViewModel registroViewModel, string? returnurl = null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            returnurl = returnurl ?? Url.Content("~/");
+
+
+            if (ModelState.IsValid)
+            {
+
+                var usuario = new UsuarioRegistrado
+                {
+                    Nombres = registroViewModel.Nombres,
+                    Apellidos = registroViewModel.Apellidos,
+                    UserName = registroViewModel.Email,
+                    Email = registroViewModel.Email,
+                    URL = registroViewModel.URL,
+                    CodigoPais = registroViewModel.CodigoPais,
+                    Pais = registroViewModel.Pais,
+                    PhoneNumber = registroViewModel.PhoneNumber,
+                    Ciudad = registroViewModel.Ciudad,
+                    Direccion = registroViewModel.Direccion,
+                    FechaNacimiento = registroViewModel.FechaNacimiento,
+                    Estado = registroViewModel.Estado
+                };
+
+                var resultado = await _userManager.CreateAsync(usuario, registroViewModel.Password); // Manejador de usuarios del Framework de Identity
+
+                if (resultado.Succeeded)
+                {
+                    //Pendiente agregar el usuario a rol por defecto
+
+
+                    // Confirmación por email...
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(usuario);
+                    var urlRetorno = Url.Action("ConfirmarEmail", "Cuentas", new { userId = usuario.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                    await _emailSender.SendEmailAsync(
+                                                        registroViewModel.Email,
+                                                        "Confirmar su cuenta - WebApplicationsEmployess",
+                                                        "Por favor confirme su cuenta dando click aquí: <a href=\"" + urlRetorno + "\">enlace</a>"
+                                                    );
+                    await _signInManager.SignInAsync(usuario, isPersistent: false);  // Manejador de autenticación del Framework de Identidad (Identity)
+                    //return LocalRedirect(returnurl);
+                    return RedirectToAction("Index", "Home");
+                }
+                ValidarErrores(resultado);
+            }
+            return View(registroViewModel);
+        }
+
+        [AllowAnonymous]
+        private void ValidarErrores(IdentityResult resultado)
+        {
+            foreach (var error in resultado.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmarEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return View("Error");
+            }
+            var usuario = await _userManager.FindByIdAsync(userId);
+            if (usuario == null)
+            {
+                return View("Error");
+            }
+            var resultado = await _userManager.ConfirmEmailAsync(usuario, code);
+            return View(resultado.Succeeded ? "ConfirmarEmail" : "Error");
+        }
+
+      
+    }
+}
---
+++ WebApplication.Monolito01/WebApplication.Monolito01/Data/Migrations/20220612185857_RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication.Monolito01.Data.Migrations
+{
+    public partial class RegisterViewModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Pais = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoPais = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuario_AspNetUsers_Id",
+                        column: x => x.Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
